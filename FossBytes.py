511048a941ece3562246d7ef20f71f393260e4db
Krishna Manohar <krishnamanohar1997@gmail.com>
2017-12-26 20:48:05 +0530

    Add files via upload

---
+++ FossBytes.py
@@ -0,0 +1,26 @@
+import urllib
+import re
+from bs4 import BeautifulSoup
+import requests
+#url = 'https://fossbytes.com/'
+url = 'https://fossbytes.com/youtube-on-fire-tv-stick-firefox-workaround/'
+headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36'}
+response = requests.get(url, headers=headers)
+print(response.status_code) 
+#print response.content
+soup = BeautifulSoup(response.content,"html.parser")
+content =  soup.find_all('div',class_="td-post-content td-pb-padding-side")
+#print soup.select('div > p')
+ allContent = (elem for elem in content[0].children if getattr(elem, 'name', None) == 'div')
+for con in content:
+	print con.text.encode('ascii','ignore')
+	print con.next_sibling
+# #print soup
+# AllLinks =[]
+# ArticleLinks =  soup.find_all("a",rel="bookmark")
+# for l in ArticleLinks:
+	# AllLinks.append(l.attrs['href']+'*'+l.attrs['title'].encode('ascii', 'ignore'))
+# #print len(AllLinks),len(set(AllLinks))
+# CleanLinks =list(set(AllLinks))
+# for li in CleanLinks:
+	# print li
