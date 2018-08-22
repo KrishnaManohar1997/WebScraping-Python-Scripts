6c67f8a3fb859e147574f17ca07bb8466ee6b8b5
Krishna Manohar <krishnamanohar1997@gmail.com>
2018-08-22 18:00:14 +0530

    Update Imdb Top 250 Movies .py

--- Imdb Top 250 Movies .py
+++ Imdb Top 250 Movies .py
@@ -8,32 +8,34 @@ for j in range(5):
 	url = 'http://www.imdb.com/search/title?groups=top_250&sort=user_rating&page='+str(j+1)+'&ref_=adv_nxt'
 	f = urllib.urlopen(url)
 	code = f.read()
-	soup = BeautifulSoup(code,"html.parser")
-	imgs = soup.find_all("img")
-	ratings = soup.find_all("div")
-	years = soup.find_all("span", class_="lister-item-year text-muted unbold")
-	#print k
-	for movieName in imgs:
-		if (movieName.attrs['alt']=='IMDbPro Menu' or movieName.attrs['alt'] == 'Go to IMDbPro'):
-			continue
-		else:
-			#print movieName.attrs['alt']
-			
-			Movies.append(str(MovieNum)+str("  ")+movieName.attrs['alt'])
-			MovieNum=MovieNum+1
-	for rats in ratings:
-		try:
-		#if(rats.attrs['name'] =='ir'):
-		#Movies.append(str(MovieNum)+str("  ")+movieName.attrs['alt'])
-			MovieRating = rats.attrs['data-value']
-			Ratings.append(MovieRating)
-		except:
-			continue
-	for year in years:
-		if year.text.find('I') == 1:
-			Years.append((year.text.lstrip('(I) (')).rstrip(')'))
-		else:
-			Years.append((year.text.lstrip('(')).rstrip(')'))
+	########################################################################
+	
+	
+	
+	Logic is Hidden for Security purposes
+	
+	
+	
+	
+	Logic is Hidden for Security purposes
+	
+	
+	
+	
+	Logic is Hidden for Security purposes
+	
+	
+	
+	
+	Logic is Hidden for Security purposes
+	
+	
+	
+	
+	##############################################################################
+	
+	
+	
 mov=0
 while mov < len(Movies):
 	print Movies[mov]+str("\t Rating ")+Ratings[mov]+str("\t Year  ")+Years[mov]
